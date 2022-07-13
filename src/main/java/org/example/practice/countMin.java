c42b6ede85e3fe9003defab2c56811f6e81da6fe
Roshan Sharma <imrosun@gmail.com>
2022-07-13 12:29:48 +0530

    Added count min moves

---
+++ src/main/java/org/example/practice/countMin.java
@@ -0,0 +1,19 @@
+package org.example.practice;
+
+import java.util.*;
+    public class countMin {
+        public static void main(String[] args) {
+            int[] arr = {1,10,29,7,5};
+            System.out.println(minMoves(arr));
+        }
+        static int minMoves(int[] nums){
+            Arrays.sort(nums);
+            int sum = 0;
+            int median = nums[nums.length/2];
+            for (int num : nums) {
+                sum = sum + Math.abs(median - num);
+            }
+            return sum;
+        }
+    }
+
